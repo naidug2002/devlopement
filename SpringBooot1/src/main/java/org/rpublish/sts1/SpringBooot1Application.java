e23110d5dfefba18999b665fcbfeff4ccdeaf71b
naidug2002 <52496090+naidug2002@users.noreply.github.com>
2019-09-22 12:59:04 +0530

    first save

---
+++ SpringBooot1/src/main/java/org/rpublish/sts1/SpringBooot1Application.java
@@ -0,0 +1,13 @@
+package org.rpublish.sts1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooot1Application.class, args);
+	}
+
+}
---
+++ SpringBooot1/src/test/java/org/rpublish/sts1/SpringBooot1ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.rpublish.sts1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBooot1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
